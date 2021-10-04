bce1cf6fb00d86c3c28cb7632dfaab9a78a31a03
Carlos <carloshrib96@gmail.com>
2021-10-03 22:33:25 -0300

    atualizando layout

--- src/App.js
+++ src/App.js
@@ -1,33 +1,76 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
+import DataTable from 'react-data-table-component';
+
 const DATABASE_URL = 'https://sictup-752d5-default-rtdb.firebaseio.com/DHT11.json?auth=X8qgzJeTWtD9bW5d7RkCxHoUdMDfrhtQvOQkEbIv';
 
 
 
 function App() {
   const [getData, setData] = useState([]);
+  const [getLoading, setLoading] = useState(true);
 
-  const fetchData = async () => {
+  const fetchData = useCallback(async () => {
     const {data} = await axios.get(DATABASE_URL);
-    const temperatures = Object.values(data.Temperatura).reverse().slice(0, 15);
-    const humidities = Object.values(data.Umidade).reverse().slice(0, 15);
+    setLoading(true)
+    const temperatures = Object.values(data.Temperatura).reverse();
+    const humidities = Object.values(data.Umidade).reverse();
+    const datetimes = Object.values(data.Horario).reverse();
     const parsedData = temperatures.map((item, index) => {
       const temperaturesAndHumidites = {
         temperature: item,
-        humidity: humidities[index]
+        humidity: humidities[index],
+        datetime: datetimes[index] || '---'
       }
 
       return temperaturesAndHumidites;
     })
-
+    
+    console.log(data);
     setData(parsedData);
-  } 
-  
+    setLoading(false)
+  }, [])
 
+  
 useEffect(() => {
   fetchData();
-}, [])
+}, [fetchData])
+
+  
+  const columnStyle = {
+      maxWidth: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '16px',
+  }
+
+  const tableColumns = [
+    {
+        name: 'Temperatura',
+        selector: row => row.temperature,
+        style: columnStyle,
+    },
+    {
+        name: 'Umidade',
+        selector: row => row.humidity,
+        style: columnStyle,
+    },
+    {
+      name: 'Horário',
+      selector: row => row.datetime,
+      style: columnStyle,
+    },
+  ];
+
+  const paginationComponentOptions = {
+    rowsPerPageText: 'Dados por página',
+    rangeSeparatorText: 'de',
+    noRowsPerPage: true,
+    
+};
+  
 
   return (
     <div className="App">
@@ -35,26 +78,22 @@ useEffect(() => {
           <h1>SICTUP</h1>
       </div>
         <div className="content">
-          <table className="dataTable">
-            <thead>
-              <tr>
-                <th><div>Temperatura</div></th>
-                <th><div>Umidade</div></th>
-              </tr>
-            </thead>
-            <tbody>
-              {getData.map((item, index) => (
-                <tr key={index}>
-                  <td>
-                    <div>{item.temperature}</div>
-                  </td>
-                  <td>
-                    <div>{item.humidity}</div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <div className="tableContent">
+            <div className="tableButtonsContent">
+              <button className="refreshDataButton" onClick={fetchData}>Atualizar Dados</button>
+            </div>
+            <DataTable
+              title="Dados coletados:"
+              columns={tableColumns}
+              data={getData}
+              pagination
+              paginationComponentOptions={paginationComponentOptions}
+              paginationPerPage={10}
+              responsive
+              noDataComponent="Nenhum dado ainda foi coletado."
+              progressPending={getLoading}
+            />
+          </div>
         </div>
     </div>
   );
